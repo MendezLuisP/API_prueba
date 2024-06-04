2fb6bc2c53982d19b51fb1e278f160883fbab326
MendezLuisP <lmendezv003@gmail.com>
2024-06-04 14:58:58 -0600

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -23,4 +24,5 @@ def create_user():
     return jsonify(data), 201 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
